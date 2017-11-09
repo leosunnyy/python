d6233bf5ee6ce0266ac3fd86e7b0de8acb9c90fa
leosunnyy <leosunnyy@gmail.com>
2017-11-09 16:55:00 +0800

    Add files via upload

---
+++ auto_login/login_info.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : login_info.py
+# @Author: leosunnyy
+# @Email: leosunnyy@gmail.com
+# @Date  : 2017/11/8
+# @Desc  :
+
+username = '798231448@qq.com'
+password = 'lzksw448'
